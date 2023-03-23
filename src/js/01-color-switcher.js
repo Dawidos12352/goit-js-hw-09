35f472bd7f51c5afc5f5866ead2436e2556b2769
Dawid Wrobel <118534055+Dawidos12352@users.noreply.github.com>
2023-03-23 17:00:25 +0100

    first task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const btnStart = document.querySelector("button[data-start]");
+const btnStop = document.querySelector("button[data-stop]");
+const body = document.querySelector("body");
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  };
+
+  let switcher = null;
+
+
+btnStart.addEventListener("click", () =>{
+btnStart.disabled = true;
+btnStop.disabled = false;
+switcher = setInterval(() => {
+body.style.backgroundColor = getRandomHexColor();
+},1000);
+});
+
+
+btnStop.addEventListener("click", () => {
+btnStart.disabled = false;
+btnStop.disabled = true;
+clearInterval(switcher);
+});
