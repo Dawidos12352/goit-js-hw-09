3470f3134b1388e4f18caf4284e05fd99bb6e3fb
Dawid Wrobel <118534055+Dawidos12352@users.noreply.github.com>
2023-03-24 11:36:28 +0100

    task 3 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
+import Notiflix from "notiflix";
+
+const inputDelay = document.querySelector(`[name="delay"]`);
+const inputStep = document.querySelector(`[name="step"]`);
+const inputAmount = document.querySelector(`[name="amount"]`);
+const btn = document.querySelector("button");
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
     // Fulfill
-  } else {
+        resolve({position, delay});
+      } else {
     // Reject
-  }
-}
+        reject({position, delay});
+      }
+    },delay);
+  });
+  return promise;
+};
+
+btn.addEventListener("click", event => {
+  event.preventDefault();
+  const firstDelay = Number(inputDelay.value);
+  const delayStep = Number(inputStep.value);
+  const amount = Number(inputAmount.value);
+
+for ( let i = 1; i <= amount; i++) {
+  let promiseDelay = firstDelay + delayStep * ( i - 1 );
+
+createPromise(i, promiseDelay)
+  .then(({ position, delay }) => {
+    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+};
+});
